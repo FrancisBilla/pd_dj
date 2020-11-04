ad5071c2fcbc918178b506f7f0af93665b10dc82
FrancisBilla <francis.billa.fb@gmail.com>
2020-11-04 09:05:25 +0000

    added data frame

--- src/products/views.py
+++ src/products/views.py
@@ -5,13 +5,25 @@ import pandas as pd
 # Create your views here.
 
 def chart_select_view(request):
+    error_message = None
+
     product_df = pd.DataFrame(Product.objects.all().values())
     purchase_df = pd.DataFrame(Purchase.objects.all().values())
     product_df['product_id'] = product_df['id']
-    df = pd.merge(purchase_df, product_df, on ='product_id').drop(['id_y','date_y'], axis=1).rename({'id_x': 'id', 'date_x': 'date'}, axis=1)
+
+    if purchase_df.shape[0] > 0:
+        df = pd.merge(purchase_df, product_df, on ='product_id').drop(['id_y','date_y'], axis=1).rename({'id_x': 'id', 'date_x': 'date'}, axis=1)
+        if request.method == 'POST':
+            chart_type = request.POST['sales']
+            date_from = request.POST['date_from']
+            date_to = request.POST['date_to']
+    else:
+        error_message = 'No records in the database'
+        df = None
     context = {
+        'error_message': error_message,
         'products': product_df.to_html(),
         'purchases': purchase_df.to_html(),
-        'df': df.to_html(),
+        'df': df,
     }
     return render(request, 'products/main.html', context)
