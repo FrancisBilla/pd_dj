de26bdb129b5e3c44c85970c10576eaa1f5a0947
FrancisBilla <francis.billa.fb@gmail.com>
2020-11-05 16:01:57 +0000

    added chart function 2

--- src/products/views.py
+++ src/products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Product, Purchase
 import pandas as pd
+from .utils import get_simple_plot
 
 # Create your views here.
 
@@ -28,6 +29,7 @@ def chart_select_view(request):
                     df = df[(df['date']> date_from) & (df['date']< date_to)]
                     df2 = df.groupby('date', as_index=False)['total_price'].agg('sum')
                 #function to get a graph
+                # get_simple_plot(chart_type, x=, y=, data=, df=)
             else:
                 error_message = 'Please select a chart type to continue'
 
