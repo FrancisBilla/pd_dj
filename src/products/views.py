9afa255b5712bbb3686ed7f50c734e8e355172e2
FrancisBilla <francis.billa.fb@gmail.com>
2020-11-06 12:47:50 +0000

    created modal with price statistics

--- src/products/views.py
+++ src/products/views.py
@@ -9,6 +9,7 @@ def chart_select_view(request):
     graph = None
     error_message = None
     df = None
+    price = None
 
     product_df = pd.DataFrame(Product.objects.all().values())
     purchase_df = pd.DataFrame(Purchase.objects.all().values())
@@ -16,11 +17,12 @@ def chart_select_view(request):
 
     if purchase_df.shape[0] > 0:
         df = pd.merge(purchase_df, product_df, on ='product_id').drop(['id_y','date_y'], axis=1).rename({'id_x': 'id', 'date_x': 'date'}, axis=1)
+        price = df['price']
         if request.method == 'POST':
             chart_type = request.POST['sales']
             date_from = request.POST['date_from']
             date_to = request.POST['date_to']
-            print(chart_type)
+            # print(chart_type)
             df['date'] = df['date'].apply(lambda x: x.strftime('%Y-%m-%d'))
             df2 = df.groupby('date', as_index=False)['total_price'].agg('sum')
 
@@ -39,6 +41,7 @@ def chart_select_view(request):
         # df = 'No records for Purchase'
 
     context = {
+        'price': price,
         'graph': graph,
         'error_message': error_message,
     }
--- src/static_project/main.js
+++ src/static_project/main.js
@@ -1,5 +1,21 @@
 $(document).ready(function(){
     $('.ui.dropdown')
         .dropdown()
+
+        $('.massage .close')
+        .on('click', function(){
+            $(this)
+            .closest('.massage')
+            .transition('fade')
+            ;
+    })
   ;
+
+    $('#modal-btn').click(function(){
+      $('.ui.modal')
+       .modal('show')
+      ;
+    })
+
+;
 })
