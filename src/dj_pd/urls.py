423e45bc751c2bc0ec3c39faa25272647b37d185
FrancisBilla <francis.billa.fb@gmail.com>
2020-11-11 11:49:50 +0000

    created purchas view

--- src/dj_pd/urls.py
+++ src/dj_pd/urls.py
@@ -22,7 +22,6 @@ from django.conf.urls.static import  static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('performance/', include('products.urls', namespace='products'))
-
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- src/products/utils.py
+++ src/products/utils.py
@@ -32,15 +32,15 @@ def get_simple_plot(chart_type, *args, **kwargs):
     data = kwargs.get('data')
     # df = kwargs.get('df')
     if chart_type == 'bar plot':
-        title = "title"
+        title = "total price by day (bar)"
         plt.title(title)
         plt.bar(x, y)
     elif chart_type == 'line plot':
-        title = "title"
+        title = "total price by day (line)"
         plt.title(title)
         plt.plot(x, y)
     else:
-        title = "title"
+        title = "Product Count"
         plt.title(title)
         sns.countplot('name', data=data)
 
