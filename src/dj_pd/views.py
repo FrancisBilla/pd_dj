1503318ec9f9ef026b0dd0f53240919f3b8e7d18
FrancisBilla <francis.billa.fb@gmail.com>
2020-11-26 18:55:18 +0000

    working on login

--- src/dj_pd/views.py
+++ src/dj_pd/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 from .forms import LoginForm
+from django.contrib.auth import authenticate, login
 
 def home_view(request):
     return render(request, 'home.html', {})
 
 
 def login_view(request):
-    return render(request, 'login.html', {})
+    error_message = None
+    form = LoginForm()
+    if request.method == 'POST':
+        form == LoginForm(data=request.POST)
+        if form.is_valid():
+            username = form.data.get('username')
+            password = form.data.get('password')
+            user = authenticate(username = username, password=password)
+            if user is not None:
+                login(request, user)
+        print(form.data)
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'login.html', context)
