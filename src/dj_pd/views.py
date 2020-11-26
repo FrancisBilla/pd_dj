4f4ec4db6aa208eb04058cb9a4ce66226a0ceac1
FrancisBilla <francis.billa.fb@gmail.com>
2020-11-26 17:41:10 +0000

    creating login form and login view

--- src/dj_pd/views.py
+++ src/dj_pd/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from .forms import LoginForm
 
 def home_view(request):
     return render(request, 'home.html', {})
+
+
+def login_view(request):
+    return render(request, 'login.html', {})
